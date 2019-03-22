451fc1b31365f1ca77ba352206169ff57ede28fe
DROUAUD Aurélien <a.drouaud@gmail.com>
2019-03-22 16:06:24 +0100

    Commit TP03 Profils PROD et DEV

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,15 +1,20 @@
 package dev;
 
 import com.github.lalyos.jfiglet.FigletFont;
-
+import java.util.ResourceBundle;
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-    	   String asciiArt = FigletFont.convertOneLine("BONJOUR  JE  SUIS  AURÉLIEN, ENCHANTÉ");
-    	    System.out.println(asciiArt);    }
+	public static void main(String[] args) {
+		String titre = ResourceBundle.getBundle("application").getString("titre");
+		String asciiArt = FigletFont.convertOneLine(titre);
+		System.out.println(asciiArt);
+		// affichage de l'environnement
+		String environnement = ResourceBundle.getBundle("application").getString(
+		"environnement");
+		System.out.println("Environnement : " + environnement);  
+		}
 }
